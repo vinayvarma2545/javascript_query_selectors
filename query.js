fca2c00d494e1bd0335d990c9652dd57a7ca6a3e
vinay <vinayvarma2545@gmail.com>
2022-08-18 01:38:07 +0530

    querySelector

---
+++ query.js
@@ -0,0 +1,6 @@
+function myFunction()
+{
+    var x = document.getElementById("gfg");
+    // x.querySelector("p").style.backgroundColor = "Green";
+    x.querySelector("p").style.color = "blue";
+}
